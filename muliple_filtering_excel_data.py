a2f46f6188d0eb2572c28ba1762865afac866f9b
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-10-16 18:50:37 +0200

    Update muliple_filtering_excel_data.py

--- muliple_filtering_excel_data.py
+++ muliple_filtering_excel_data.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import sys
-#this script takes as input: data to be filtered,excel with filters in sheets
-#and gives as output an excel with the filtered data in the current working directory
+#this script takes as input: data to be filtered in excel file,excel file with filters in sheets
+#and gives as output an excel file with the filtered data in the current working directory
 
-#defining a function that takes data from excel and applies conditional filrering to columns based on a list of dictionaries
+#defining a function that takes data from excel files and applies conditional filtering to columns based on a list of dictionaries
 #the keys are the columns you apply a filter on and the values are threshold/filter
-#2 dictionaries in the list: 1 for columns to be filtered for greater than/equal to a value & 2 for columns to be filtered for less than/equal to a value
+#2 dictionaries in the list: 1)for columns to be filtered for greater than/equal to a value and 2)for columns to be filtered for less than/equal to a value
 def filter(data,dict_list):
     greater = dict_list[0]
     lower = dict_list[1]
@@ -27,5 +27,5 @@ for key, value in sheet_dict.items():
 
 #apply our function
 filtered_data = filter(data, dict_list)
-print(filtered_data)
-filtered_data.to_excel(excel_writer='filtered_data.xlsx', index=False)
+#save the filtered data to an excel file to the current working directory 
+filtered_data.to_excel(excel_writer='filtered_data.xlsx', index=False, sheet_name='Data')
