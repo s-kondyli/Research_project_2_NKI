b123a2edf9bc317c8628edd9fc11408c9534e2df
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-10-18 22:21:21 +0200

    Update finding_mutual_values.py

--- finding_mutual_values.py
+++ finding_mutual_values.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import sys
-#we want to find the mutual values from 2 dataframes
-
-data = pd.read_excel(sys.argv[1]) 
-df = pd.read_excel(sys.argv[2], names=['patient'])
+# select values from a dataframe based on a list 
+data = pd.read_excel(sys.argv[1])
 data.fillna(value=0, inplace=True)
+initial_patients = data['HiX#'].size
+df = pd.read_excel(sys.argv[2], names=['patient'])
 
 check = df['patient'].tolist()
-data = data[data['HiX#'].isin(check)] #we keep only the values of data['HiX#'] that are the in the list check 
+data = data[data['HiX#'].isin(check)] #select only the values of the dataframe that are in the list 
+mutual_patients = data['HiX#'].size
+difference = initial_patients - mutual_patients
+print("Initial patients:",initial_patients,"\nMutual patients:",mutual_patients,"\nDifference:",difference )
+data.to_excel(excel_writer='mutual_patients.xlsx', index=False, sheet_name='Mutual_Data')
 
 
