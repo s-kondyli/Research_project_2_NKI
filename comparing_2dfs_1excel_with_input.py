f7e1a09258e5ea24b936510e441f31ae1f5dc4f2
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-10-30 21:02:43 +0100

    Update comparing_2dfs_1excel_with_input.py

--- comparing_2dfs_1excel_with_input.py
+++ comparing_2dfs_1excel_with_input.py
@@ -2,23 +2,49 @@
 # input in command line: the excel workbook
 # input during the running of the script: sheetnames to be compared and the column name that they will be compared on
 # output: excel workbook in the cwd with 3 sheets: the mutual values, unique in df1, unique in df2
-
-
 import pandas as pd
-import sys
+import os
 import time
-timestr = time.strftime("%Y%m%d%H%M") #optional how many details you include on the datetime: you could only include month_year ("%Y%m")
+path = os.getcwd() #get the absolute path to the current working directory
+files = os.listdir(path) #list of files in the cwd
+excel_files = [f for f in files if f[-4:] == 'xlsx'] #only excel files in the cwd
+timestr = time.strftime("%Y%m%d%H%M") #time string for the output file
 
-#request data 
-sheetname1 = input('First sheetname for the comparison:')
-sheetname2 = input('Second sheetname for the comparison:')
-column_to_compare = input('column name to compare the files:')
+#request data
+while True:
+    try:
+        workbook = input("Enter the excel workbook:")
+        if workbook in excel_files:
+            break
+        else:
+            print(workbook,'is not a valid name.\nValid files are:', excel_files)
+    except Exception:
+        print(workbook,'is not a valid name.\nValid files are:', excel_files)
 
-#import data
-sheet_dict = pd.read_excel(sys.argv[1], sheet_name=None)
+sheet_dict = pd.read_excel(workbook, sheet_name=None)
+while True:
+    sheetname1 = input("Enter 1st sheetname containing data for comparison:")
+    if sheetname1 not in sheet_dict.keys():
+        print("Sheetname not valid.\nValid sheetnames are:", sheet_dict.keys())
+    else:
+        break
+while True:
+    sheetname2 = input("Enter 2nd sheetname containing data for comparison:")
+    if sheetname2 not in sheet_dict.keys():
+        print("Sheetname not valid.\nValid sheetnames are:", sheet_dict.keys())
+    else:
+        break
+
 #assign the values of the dict to the corresponding dfs based on the sheetname
 df1 = sheet_dict.get(sheetname1)
 df2 = sheet_dict.get(sheetname2)
+while True:
+    column_to_compare = input("Enter mutual column name to compare the dfs:")
+    if column_to_compare not in df1.columns and column_to_compare not in df2.columns:
+        print("Sheetname not valid. Try again using the mutual column name of the dfs")
+    else:
+        break
+
 # Clean up data
 df1.dropna(how='all', inplace=True) # drop the rows where in all columns of the df the values are NaN, so keep rows that NaN are only in some columns
 df2.dropna(how='all', inplace=True)
@@ -46,3 +72,5 @@ frames = {'Mutual_patients': mutual, sheetname1: df1only, sheetname2: df2only}
 with pd.ExcelWriter('BL_samples_compared_'+ timestr +'_.xlsx') as writer:
     for sheet, frame in frames.items():
         frame.to_excel(writer, sheet_name=sheet, index=False)
+
+print("All done!")
